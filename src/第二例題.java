0272f9d17cb0c977a2004ee43f436f827e99698f
Bryan8702136 <105053078@live.asia.edu.tw>
2017-10-02 15:56:40 +0800

    20171002課堂作業

--- src/第二例題.java
+++ src/第二例題.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
+public class 第二例題 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char v = sc.next().charAt(0);
         if( v - 'A' >= 0 && v - 'A' <= 26){
+            System.out.println("大寫");
         }
         else if(v -'a' >= 0 && v - 'a' <= 26){
+            System.out.println("小寫");
         }
         else{
             System.out.println("非英文字母");
